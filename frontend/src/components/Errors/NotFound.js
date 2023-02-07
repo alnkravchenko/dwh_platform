c967b5163c067f954d36258ed2d6be7c6c7fdf15
alnkravchenko <alenakravchenko1@gmail.com>
2023-02-07 09:49:58 +0200

    add NotFound page

--- frontend/src/components/Errors/NotFound.js
+++ frontend/src/components/Errors/NotFound.js
@@ -1,5 +1,17 @@
+import { Alert, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Errors.scss";
+
 const NotFound = () => {
-  return <h1>NOT FOUND</h1>;
+  return (
+    <Alert variant="warning" className="centered message-box">
+      <Alert.Heading className="header">PAGE NOT FOUND</Alert.Heading>
+      <p>It looks like you got a wrong URL!</p>
+      <Link to="/">
+        <Button variant="outline-primary">Go back home</Button>
+      </Link>
+    </Alert>
+  );
 };
 
 export default NotFound;
