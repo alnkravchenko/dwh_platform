888e587afb372c959b18a5a699c2287cac4816b3
alnkravchenko <alenakravchenko1@gmail.com>
2023-02-27 13:02:50 +0200

    [backend] add login and sign verification implementation

--- backend/src/repos/user.py
+++ backend/src/repos/user.py
@@ -11,10 +11,18 @@ def get_users(db: Session, offset: int = 0, limit: int = 100) -> List[UserDB]:
     return db.query(UserDB).offset(offset).limit(limit).all()
 
 
+def get_user_by_username(db: Session, user: UserModel) -> List[UserDB]:
+    return db.query(UserDB).filter_by(username=user.username).all()
+
+
+def get_user_by_email(db: Session, user: UserModel) -> List[UserDB]:
+    return db.query(UserDB).filter_by(email=user.email).all()
+
+
 def create_user(db: Session, user: UserModel) -> UserDB:
     hashed_password = get_hashed_password(user.password)
     db_user = UserDB(username=user.username, email=user.email, password=hashed_password)
-    return create_entity(db, db_user)
+    return create_entity(db, db_user)  # type: ignore
 
 
 def create_entity(db: Session, entity: Base) -> Base:
--- backend/src/utils/verification.py
+++ backend/src/utils/verification.py
@@ -1,10 +1,16 @@
+from repos import user as user_db
 from schema.user import UserModel
 from sqlalchemy.orm import Session
 
 
 def verify_user(db: Session, user: UserModel) -> bool:
-    return True
+    user_entities = user_db.get_user_by_email(db, user)
+    if len(user_entities) == 1:
+        return user.password == user_entities[0].password
+    return False
 
 
 def verify_new_user(db: Session, user: UserModel) -> bool:
-    return True
+    users_by_username = user_db.get_user_by_username(db, user)
+    users_by_email = user_db.get_user_by_email(db, user)
+    return len(users_by_username) + len(users_by_email) == 0
