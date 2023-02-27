1391c8fbe95543f51228f04cb5dc0ac60b9a73ac
alnkravchenko <alenakravchenko1@gmail.com>
2023-02-27 13:02:06 +0200

    [backend] add error response to sign up endpoint

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -19,6 +19,7 @@ def login(
     if verify_user(db, user):
         user_dict = user.dict()
         user_dict["response"] = "LOGIN"
+
         log.info(user_dict)
         response.status_code = status.HTTP_200_OK
         return user_dict
@@ -33,11 +34,16 @@ def get_all_users(offset: int = 0, limit: int = 100, db: Session = Depends(get_d
 
 
 @router.post("/sign_up")
-def sign_up(user: UserModel, db: Session = Depends(get_db)) -> Dict[str, str]:
+def sign_up(
+    user: UserModel, response: Response, db: Session = Depends(get_db)
+) -> Dict[str, str]:
     if verify_new_user(db, user):
         user_dict = user.dict()
         user_dict["response"] = "SIGN UP"
+
         log.info(user_dict)
         create_user(db, user)
+        return user_dict
 
-    return user_dict
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    return {"detail": "Invalid data"}
