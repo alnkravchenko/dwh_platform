c7b61e908618b613b1235d49dec42db8435601d7
alnkravchenko <alenakravchenko1@gmail.com>
2023-02-27 12:59:36 +0200

    add usage of Auth generic page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./components/AuthenticationPage/Auth";
 import NotFound from "./components/Errors/NotFound";
-import Login from "./components/LoginPage/Login";
-import SignUp from "./components/SignUpPage/SignUp";
+import SignUpButton from "./components/SignUpButton/SignUp";
+import AuthService from "./services/auth";
+
+const authService = new AuthService();
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route path="/sing_up" element={<SignUp />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <Auth
+                pageName="Login"
+                authFunc={authService.loginUser}
+                extraComponent={<SignUpButton />}
+              />
+            }
+          />
+          <Route
+            path="/sing_up"
+            element={
+              <Auth pageName="Sign Up" authFunc={authService.signUpUser} />
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
