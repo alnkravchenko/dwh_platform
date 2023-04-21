7f2d58b5cda7f6f4111fb98cbf9bb94b0a3fcf52
alnkravchenko <alenakravchenko1@gmail.com>
2023-04-21 20:27:36 +0300

    [backend] add JWT generation to login and sign up

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -1,18 +1,20 @@
-from typing import List
-
 import structlog
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer
 from repos.database import get_db
-from repos.user import create_user, get_users
-from schema.responses import DefaultResponse
+from repos.users import create_user
+from schema.jwt_auth import Token
+from schema.responses import DefaultResponse, TokenResponse
 from schema.user import UserModel
 from sqlalchemy.orm import Session
+from utils import jwt_auth
+from utils import verification as verif
 from utils.validation import validate_password
-from utils.verification import verify_new_user, verify_user
 
 log = structlog.get_logger(module=__name__)
 router = APIRouter(prefix="/auth", tags=["auth"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 
 @router.post(
@@ -20,23 +22,26 @@ router = APIRouter(prefix="/auth", tags=["auth"])
     responses={200: {"model": DefaultResponse}, 401: {"model": DefaultResponse}},
 )
 def login(user: UserModel, db: Session = Depends(get_db)) -> JSONResponse:
-    is_verified, msg = verify_user(db, user)
+    is_verified, msg = verif.verify_user(db, user)
     status_code = 200 if is_verified else 401
-    log.info(f"[LOGIN] {status_code} {msg}")
-    return JSONResponse(content={"details": msg}, status_code=status_code)
-
-
-@router.get("/users/", response_model=List[UserModel])
-def get_all_users(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return get_users(db, offset, limit)
+    log.info(f"[LOGIN | GETTING TOKEN] {status_code} {msg}")
+    # create response
+    response_content = {"details": msg}
+    if status_code == 200:
+        user_token = Token(
+            access_token=jwt_auth.create_access_token(user.email),
+            token_type="bearer",
+        )
+        response_content.update(token=user_token.json())
+    return JSONResponse(content=response_content, status_code=status_code)
 
 
 @router.post(
     "/sign_up",
-    responses={201: {"model": DefaultResponse}, 400: {"model": DefaultResponse}},
+    responses={201: {"model": TokenResponse}, 400: {"model": DefaultResponse}},
 )
 def sign_up(user: UserModel, db: Session = Depends(get_db)) -> JSONResponse:
-    is_verified = verify_new_user(db, user)
+    is_verified = verif.verify_new_user(db, user)
     is_validated = validate_password(user.password)
     sign_up_flag = all([is_verified[0], is_validated[0]])
 
@@ -48,4 +53,12 @@ def sign_up(user: UserModel, db: Session = Depends(get_db)) -> JSONResponse:
     ).strip()
     status_code = 201 if sign_up_flag else 400
     log.info(f"[SIGN UP] {status_code} {msgs}")
-    return JSONResponse(content={"details": msgs}, status_code=status_code)
+    # create response
+    response_content = {"details": msgs}
+    if status_code == 201:
+        user_token = Token(
+            access_token=jwt_auth.create_access_token(user.email),
+            token_type="bearer",
+        )
+        response_content.update(token=user_token.json())
+    return JSONResponse(content=response_content, status_code=status_code)
