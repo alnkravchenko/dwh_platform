3095dc32e9d38307281eb5bcc3ad02cd12369629
alnkravchenko <alenakravchenko1@gmail.com>
2023-04-25 15:03:00 +0300

    [backend] add db commit to sql queries, change return types

--- backend/src/repos/projects.py
+++ backend/src/repos/projects.py
@@ -2,28 +2,37 @@ from typing import List
 from uuid import UUID
 
 from models.project import ProjectDB
-from schema.project import ProjectContent
+from schema.project import ProjectContent, ProjectCreate, ProjectModel, ProjectUpdate
 from schema.user import UserModel
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 
 def get_user_projects(
-    db: Session, user: UserModel, offset: int = 0, limit: int = 5
-) -> List[ProjectDB]:
+    db: Session, user: UserModel, offset: int = 0, limit: int = 25
+) -> List[ProjectModel]:
     query = (
         select(ProjectDB)
         .where(ProjectDB.created_by == user.id)
         .offset(offset)
         .limit(limit)
     )
-    return list(db.execute(query).scalars().all())
+    projects = db.execute(query).scalars().all()
+    db.commit()
+    if len(projects) > 0:
+        return list(map(ProjectModel.from_orm, projects))
+    return []
 
 
-def get_content(db: Session, project_id: UUID) -> ProjectContent:
-    project_query = select(ProjectDB).where(ProjectDB.id == project_id)
-    project_db = db.execute(project_query).scalar()
-    return ProjectContent.from_orm(project_db)
+def get_content(db: Session, user_id: UUID, project_id: UUID) -> ProjectContent | None:
+    query = (
+        select(ProjectDB)
+        .where(ProjectDB.created_by == user_id)
+        .where(ProjectDB.id == project_id)
+    )
+    project_db = db.execute(query).scalar()
+    if project_db is not None:
+        return ProjectContent.from_orm(project_db)
     # ds_query = select(DatasourceDB).where(DatasourceDB.project_id == project_id)
     # datasources: List[DatasourceModel] = list(db.execute(ds_query).scalars().all())
 
--- backend/src/repos/users.py
+++ backend/src/repos/users.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from models.user import UserDB
-from schema.user import UserModel
+from schema.user import UserCreate, UserModel
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from utils.hashing import get_hashed_password
@@ -9,24 +9,32 @@ from utils.hashing import get_hashed_password
 from .database import create_entity
 
 
-def get_users(db: Session, offset: int = 0, limit: int = 100) -> List[UserDB]:
+def get_users(db: Session, offset: int = 0, limit: int = 100) -> List[UserModel]:
     query = select(UserDB).offset(offset).limit(limit)
-    return list(db.execute(query).scalars().all())
+    users = db.execute(query).scalars().all()
+    db.commit()
+    return list(map(UserModel.from_orm, users))
 
 
-def get_user_by_username(db: Session, username: str) -> UserDB | None:
+def get_user_by_username(db: Session, username: str) -> UserModel | None:
     query = select(UserDB).where(UserDB.username == username)
-    return db.execute(query).scalar()
+    user_db = db.execute(query).scalar()
+    db.commit()
+    if user_db is not None:
+        return UserModel.from_orm(user_db)
 
 
-def get_user_by_email(db: Session, user_email: str) -> UserDB | None:
+def get_user_by_email(db: Session, user_email: str) -> UserModel | None:
     query = select(UserDB).where(UserDB.email == user_email)
-    return db.execute(query).scalar()
+    user_db = db.execute(query).scalar()
+    db.commit()
+    if user_db is not None:
+        return UserModel.from_orm(user_db)
 
 
-def create_user(db: Session, user: UserModel) -> UserDB:
+def create_user(db: Session, user: UserCreate) -> UserModel:
     hashed_password = get_hashed_password(user.password.get_secret_value())
-    db_user = UserDB(
+    user_db = UserDB(
         username=user.email.name, email=user.email.email, password=hashed_password
     )
-    return create_entity(db, db_user)  # type: ignore
+    return UserModel.from_orm(create_entity(db, user_db))
