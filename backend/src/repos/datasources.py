25bd960d58874803779216b082ec9b0a94d98965
alnkravchenko <alenakravchenko1@gmail.com>
2023-05-26 12:46:57 +0300

    [backend] add requests for datasource owner and all user datasources

--- backend/src/repos/datasources.py
+++ backend/src/repos/datasources.py
@@ -1,10 +1,31 @@
+from typing import List
 from uuid import UUID
 
 from models.datasource import DatasourceDB
+from models.project import ProjectDB
+from models.user import UserDB
 from repos.database import create_entity
 from schema.datasource import DatasourceCreate, DatasourceModel, DatasourceUpdate
+from schema.user import UserModel
 from sqlalchemy import delete, select, update
 from sqlalchemy.orm import Session
+
+
+def get_user_datasources(
+    db: Session, user_id: UUID, offset: int = 0, limit: int | None = 25
+) -> List[DatasourceModel]:
+    query = (
+        select(DatasourceDB)
+        .join(ProjectDB, DatasourceDB.project_id == ProjectDB.id)
+        .where(ProjectDB.created_by == user_id)
+        .offset(offset)
+        .limit(limit)
+    )
+    ds_db = db.execute(query).scalars().all()
+    db.commit()
+    if len(ds_db) > 0:
+        return list(map(DatasourceModel.from_orm, ds_db))
+    return []
 
 
 def get_datasource_by_id(db: Session, ds_id: UUID) -> DatasourceModel | None:
@@ -13,6 +34,19 @@ def get_datasource_by_id(db: Session, ds_id: UUID) -> DatasourceModel | None:
     db.commit()
     if ds_db is not None:
         return DatasourceModel.from_orm(ds_db)
+
+
+def get_datasource_owner(db: Session, ds_id: UUID) -> UserModel | None:
+    query = (
+        select(UserDB)
+        .join(ProjectDB, UserDB.id == ProjectDB.created_by)
+        .join(DatasourceDB, ProjectDB.id == DatasourceDB.project_id)
+        .where(DatasourceDB.id == ds_id)
+    )
+    owner_db = db.execute(query).scalar()
+    db.commit()
+    if owner_db is not None:
+        return UserModel.from_orm(owner_db)
 
 
 def create_datasource(db: Session, ds: DatasourceCreate) -> DatasourceModel:
