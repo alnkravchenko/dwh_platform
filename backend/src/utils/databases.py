3065d6ebf05c77307fbd8e07704e20c780f3b6da
alnkravchenko <alenakravchenko1@gmail.com>
2023-05-30 10:33:28 +0300

    [backend] add connection creation for db utils and loading data to file

--- backend/src/utils/databases.py
+++ backend/src/utils/databases.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from typing import Dict, List, Tuple, TypedDict
 from urllib.parse import urlparse
 
@@ -14,6 +16,45 @@ class ColumnInfo(TypedDict):
 class TableInfo(TypedDict):
     table_name: str
     columns: List[ColumnInfo]
+
+
+def create_postgres_connection(url: str):
+    return psycopg2.connect(url)
+
+
+def parse_mysql_url(url: str) -> Dict[str, str]:
+    parsed_url = urlparse(url)
+    username = parsed_url.username
+    password = parsed_url.password
+    hostname = parsed_url.hostname
+    port = parsed_url.port
+    database = parsed_url.path.strip("/")
+
+    connection_dict = {
+        "user": username,
+        "password": password,
+        "host": hostname,
+        "port": port,
+        "database": database,
+    }
+    return connection_dict
+
+
+def create_mysql_connection(url: str):
+    return mysql.connector.connect(**parse_mysql_url(url))
+
+
+def parse_mongodb_url(url: str) -> Tuple[str, str]:
+    db_name = url.split("/")[-1]
+    url = url.split(db_name)[0][:-1]
+    return url, db_name
+
+
+def create_mongodb_connection(url: str):
+    url, db_name = parse_mongodb_url(url)
+    # establish connection
+    client = MongoClient(url)
+    return client, db_name
 
 
 def parse_data_type_mongodb(data_type: str) -> str:
@@ -63,7 +104,7 @@ def parse_data_type_postgres(data_type: str) -> str:
 
 
 def get_postgres_tables(url: str) -> List[str]:
-    conn = psycopg2.connect(url)
+    conn = create_postgres_connection(url)
     # establish connection
     cursor = conn.cursor()
     cursor.execute(
@@ -81,7 +122,7 @@ def get_postgres_tables_info(
 ) -> List[Dict[str, str | List[Dict[str, str]]]]:
     table_info = []
     # establish connection
-    conn = psycopg2.connect(url)
+    conn = create_postgres_connection(url)
     cursor = conn.cursor()
     for table_name in tables:
         cursor.execute(
@@ -102,27 +143,9 @@ def get_postgres_tables_info(
     return table_info
 
 
-def parse_mysql_url(url: str) -> Dict[str, str]:
-    parsed_url = urlparse(url)
-    username = parsed_url.username
-    password = parsed_url.password
-    hostname = parsed_url.hostname
-    port = parsed_url.port
-    database = parsed_url.path.strip("/")
-
-    connection_dict = {
-        "user": username,
-        "password": password,
-        "host": hostname,
-        "port": port,
-        "database": database,
-    }
-    return connection_dict
-
-
 def get_mysql_tables(url: str) -> List[str]:
     # establish connection
-    conn = mysql.connector.connect(**parse_mysql_url(url))
+    conn = create_mysql_connection(url)
     cursor = conn.cursor()
     cursor.execute("SHOW TABLES")
     tables = cursor.fetchall()
@@ -135,11 +158,10 @@ def get_mysql_tables(url: str) -> List[str]:
 def get_mysql_tables_info(url: str, tables: List[str]):
     table_info = []
     # establish connection
-    conn = mysql.connector.connect(**parse_mysql_url(url))
+    conn = create_mysql_connection(url)
     cursor = conn.cursor()
     for table in tables:
-        table_name = table[0]
-        cursor.execute(f"SHOW COLUMNS FROM {table_name};")
+        cursor.execute(f"SHOW COLUMNS FROM {table};")
         columns = cursor.fetchall()
         # parse results
         column_info = [
@@ -149,23 +171,16 @@ def get_mysql_tables_info(url: str, tables: List[str]):
             }
             for column in columns
         ]
-        table_info.append({"name": table_name, "columns": column_info})
+        table_info.append({"table_name": table, "columns": column_info})
     # close connection
     cursor.close()
     conn.close()
     return table_info
 
 
-def parse_mongodb_url(url: str) -> Tuple[str, str]:
-    db_name = url.split("/")[-1]
-    url = url.split(db_name)[0][:-1]
-    return url, db_name
-
-
 def get_mongodb_tables(url: str):
-    url, db_name = parse_mongodb_url(url)
     # establish connection
-    client = MongoClient(url)
+    client, db_name = create_mongodb_connection(url)
     db = client[db_name]
     tables = db.list_collection_names()
     # close connection
@@ -175,9 +190,8 @@ def get_mongodb_tables(url: str):
 
 def get_mongodb_tables_info(url: str, colls: List[str]):
     table_info = []
-    url, db_name = parse_mongodb_url(url)
     # establish connection
-    client = MongoClient(url)
+    client, db_name = create_mongodb_connection(url)
     db = client[db_name]
     for collection_name in colls:
         collection = db[collection_name]
@@ -188,8 +202,9 @@ def get_mongodb_tables_info(url: str, colls: List[str]):
             if key == "_id":
                 continue
             column_info.append({"name": key, "type": type(value).__name__})
-        table_info.append({"name": collection_name, "columns": column_info})
+        table_info.append({"table_name": collection_name, "columns": column_info})
     # close connection
+    client.close()
     return table_info
 
 
@@ -206,5 +221,64 @@ def get_tables(database_url: str, tables: List[str]) -> List[TableInfo]:
             # tables = get_tables(database_url)
             tables_info = get_tables_info(database_url, tables)
             return tables_info
+
+    raise ValueError("Unsupported database URL")
+
+
+def read_from_postgres(url: str, table_name: str):
+    conn = create_postgres_connection(url)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    data = cursor.fetchall()
+    # Write data to a file
+    filepath = os.path.join("tmp", f"{uuid.uuid4().hex}.csv")
+    with open(filepath, "w") as file:
+        for row in data:
+            file.write(",".join(map(str, row)) + "\n")
+    cursor.close()
+    conn.close()
+    return filepath
+
+
+def read_from_mysql(url: str, table_name: str):
+    conn = create_mysql_connection(url)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    data = cursor.fetchall()
+    # Write data to a file
+    filepath = os.path.join("tmp", f"{uuid.uuid4().hex}.csv")
+    with open(filepath, "w") as file:
+        for row in data:
+            file.write(",".join(map(str, row)) + "\n")
+    cursor.close()
+    conn.close()
+    return filepath
+
+
+def read_from_mongodb(url: str, table_name: str) -> str:
+    client, db_name = create_mongodb_connection(url)
+    db = client[db_name]
+    collection = db[table_name]
+    data = collection.find()
+    # Write data to a file
+    filepath = os.path.join("tmp", f"{uuid.uuid4().hex}.json")
+    with open(filepath, "w") as file:
+        for document in data:
+            file.write(str(document) + "\n")
+    client.close()
+    return filepath
+
+
+def read_from_db(url: str, table_name: str) -> str:
+    database_functions = {
+        "postgres://": read_from_postgres,
+        "postgresql://": read_from_postgres,
+        "mysql://": read_from_mysql,
+        "mongodb://": read_from_mongodb,
+    }
+    for prefix, read_from in database_functions.items():
+        if url.startswith(prefix):
+            file_path = read_from(url, table_name)
+            return file_path
 
     raise ValueError("Unsupported database URL")
